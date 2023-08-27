6d736bf1b13bafe4ca56ad7c7465f6bf7b5484aa
Roman Grishin <roman.grishin.me@gmail.com>
2023-08-28 00:21:46 +0600

    hosts

--- app.js
+++ app.js
@@ -1,12 +1,21 @@
 const express = require('express');
+const mongoose = require('mongoose');
+
+const userRoutes = require('./routes/users');
 
 const { PORT = 3000 } = process.env;
 const app = express();
 
+mongoose.connect('mongodb://127.0.0.1:27017/mesto', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+app.use('/users', userRoutes);
 app.use('/', (req, res) => {
   res.send({ message: '123' });
 });
 
 app.listen(PORT, () => {
-  console.log('123');
+  console.log(PORT, 'im working');
 });
--- controllers/users.js
+++ controllers/users.js
@@ -5,13 +5,14 @@ module.exports.findAllUsers = () => {
 };
 
 module.exports.findUserById = (req, res) => {
-  const { id } = req.body.id;
+  const id = req.params.userId;
   User.findById(id)
     .then((user) => res.send({ data: user }))
     .catch((err) => res.status(500).send({ message: err }));
 };
 
 module.exports.createUser = (req, res) => {
+  console.log(1);
   const { name, about, avatar } = req.body;
   User.create({ name, about, avatar })
     .then((user) => res.send({ data: user }))
