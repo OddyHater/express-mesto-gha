6bc96b701a22507f08e5cc17d622d2ce2d19cf45
Roman Grishin <roman.grishin.me@gmail.com>
2023-08-31 23:38:47 +0600

    email validation

--- models/user.js
+++ models/user.js
@@ -49,6 +49,13 @@ const userSchema = mongoose.Schema({
   avatar: {
     type: String,
     default: 'https://pictures.s3.yandex.net/resources/jacques-cousteau_1604399756.png',
+    validate: {
+      validator: (value) => {
+        const urlPattern = /^(http:\/\/www\.|https:\/\/www\.|http:\/\/|https:\/\/)?[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$/;
+        return urlPattern.test(value);
+      },
+      message: 'Некорректный URL для аватара',
+    },
   },
 });
 
