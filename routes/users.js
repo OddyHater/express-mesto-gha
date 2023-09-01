573543ee3027a206565c55a5633885b94d3201dc
oddyhater <roman.grishin.me@gmail.com>
2023-09-01 11:24:56 +0600

    user require validation test

--- routes/users.js
+++ routes/users.js
@@ -17,11 +17,36 @@ const findAllUsersValidation = celebrate({
   }),
 });
 
+const getCurrentUserValidataion = celebrate({
+  params: Joi.object().keys({
+    userId: Joi.string(),
+  }),
+});
+
+const updateProfileValidation = celebrate({
+  params: Joi.object.keys({
+    id: Joi.string(),
+  }),
+  body: Joi.object().keys({
+    name: Joi.string().required().min(2).max(30),
+    about: Joi.string().required().min(2).max(30),
+  }),
+});
+
+const updateAvatarValidation = celebrate({
+  params: Joi.object.keys({
+    id: Joi.string(),
+  }),
+  body: Joi.object().keys({
+    avatar: Joi.link(),
+  }),
+});
+
 router.get('/users', findAllUsersValidation, findAllUsers);
 
-router.get('/users/me', getCurrentUser);
-router.get('/users/:userId', findUserById);
-router.patch('/users/me', updateProfile);
-router.patch('/users/me/avatar', updateAvatar);
+router.get('/users/me', getCurrentUserValidataion, getCurrentUser);
+router.get('/users/:userId', findAllUsersValidation, findUserById);
+router.patch('/users/me', updateProfileValidation, updateProfile);
+router.patch('/users/me/avatar', updateAvatarValidation, updateAvatar);
 
 module.exports = router;
